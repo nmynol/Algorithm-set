ce70448451e872072b5874fa91be70966ee19940
nmynol <nmynol@outlook.com>
2020-05-27 15:22:21 +0800

    map1

--- 3.2BinarySearchingTree/bst.cpp
+++ 3.2BinarySearchingTree/bst.cpp
@@ -1,3 +1,8 @@
+
+/*
+    能用于任何对象的map（二叉搜索树实现）
+*/
+
 #include<iostream>
 using namespace std;
 
@@ -37,29 +42,47 @@ public:
     }
     bool equal(Key a, Key b)
     {
-        return a = b;
+        return a == b;
     }
-    bst()                                                                   //构造函数
+    bst()                                                                   // 构造函数
     {
         root = nullptr;
     }
-    int size();                                                             //返回树的结点数
-    int size(Node<Key, Value> *p);                                          //返回以这个结点为根结点的子树的结点数
-    Value get(Key key);                                                     //返回键对应的值
+    int size();                                                             // 返回树的结点数
+    int size(Node<Key, Value> *p);                                          // 返回以这个结点为根结点的子树的结点数
+    Value get(Key key);                                                     // 返回键对应的值
     Value get(Node<Key, Value> *root, Key key); 
-    void put(Key key, Value val);                                           //插入键值对
+    void put(Key key, Value val);                                           // 插入键值对
     Node<Key, Value>* put(Node<Key, Value> *root, Key key, Value val);
-    // void xianxu()
-    // {
-    //     xianxu(root);
-    // }
-    // void xianxu(Node<Key, Value> *root)
-    // {
-    //     if(root == nullptr) return;
-    //     cout << root->key << ' ' << root->val << endl;
-    //     xianxu(root->left);
-    //     xianxu(root->right);
-    // }
+    Key min();                                                              // 返回最小键
+    Node<Key, Value>* min(Node<Key, Value>* root);
+    Key max();                                                              // 返回最大键
+    Node<Key, Value>* max(Node<Key, Value>* root);
+    Key floor(Key key);                                                     // 小于等于键的最大键
+    Node<Key, Value>* floor(Node<Key, Value>* root, Key key);
+    Key ceiling(Key key);                                                   // 大于等于键的最小键
+    Node<Key, Value>* ceiling(Node<Key, Value>* root, Key key);
+    Key select(int k);                                                      // 返回排名为k的键（有k-1个键小于他）
+    Node<Key, Value>* select(Node<Key, Value>* root, int k);
+    int rank(Key key);                                                      // 返回排名为k的键（有k-1个键小于他）
+    int rank(Node<Key, Value>* root, Key key);
+    void deleteMin();                                                       // 删除最小键的结点
+    Node<Key, Value>* deleteMin(Node<Key, Value>* root);
+    void deleteMax();                                                       // 删除最大键的结点
+    Node<Key, Value>* deleteMax(Node<Key, Value>* root);
+    void Delete(Key key);                                                   // 删除指定结点
+    Node<Key, Value>* Delete(Node<Key, Value>* root, Key key);
+    void xianxu()
+    {
+        xianxu(root);
+    }
+    void xianxu(Node<Key, Value> *root)
+    {
+        if(root == nullptr) return;
+        cout << root->key << ' ' << root->val << endl;
+        xianxu(root->left);
+        xianxu(root->right);
+    }
 };
 template<class Key, class Value>
 int bst<Key, Value>::size()
@@ -100,17 +123,158 @@ Node<Key, Value>* bst<Key, Value>::put(Node<Key, Value> *root, Key key, Value va
     root->N = size(root->left) + size(root->right) + 1;
     return root;
 }
-
+template<class Key, class Value>
+Key bst<Key, Value>::min()
+{
+    return min(root)->key;
+}
+template<class Key, class Value>
+Node<Key, Value>* bst<Key, Value>::min(Node<Key, Value>* root)
+{
+    if(root->left == nullptr) return root;
+    return min(root->left);
+}
+template<class Key, class Value>
+Key bst<Key, Value>::max()
+{
+    return max(root)->key;
+}
+template<class Key, class Value>
+Node<Key, Value>* bst<Key, Value>::max(Node<Key, Value>* root)
+{
+    if(root->right == nullptr) return root;
+    return max(root->right);
+}
+template<class Key, class Value>
+Key bst<Key, Value>::floor(Key key)
+{
+    Node<Key, Value>* x = floor(root, key);
+    if(!x) 
+    {
+        cout << "can't find" << endl;
+        return NULL;
+    }
+    return x->key;
+}
+template<class Key, class Value>
+Node<Key, Value>* bst<Key, Value>::floor(Node<Key, Value>* root, Key key)
+{
+    if(!root) return nullptr;
+    if(equal(root->key, key))return root;
+    else if(more(root->key, key))return floor(root->left, key);
+    Node<Key, Value>* t = floor(root->right, key);
+    if(t) return t;
+    else return root;
+}
+template<class Key, class Value>
+Key bst<Key, Value>::ceiling(Key key)
+{
+    Node<Key, Value>* x = ceiling(root, key);
+    if(!x) 
+    {
+        cout << "can't find" << endl;
+        return NULL;
+    }
+    return x->key;
+}
+template<class Key, class Value>
+Node<Key, Value>* bst<Key, Value>::ceiling(Node<Key, Value>* root, Key key)
+{
+    if(!root) return nullptr;
+    if(equal(root->key, key))return root;
+    else if(less(root->key, key))return ceiling(root->right, key);
+    Node<Key, Value>* t = ceiling(root->left, key);
+    if(t) return t;
+    else return root;
+}
+template<class Key, class Value>
+Key bst<Key, Value>::select(int k)
+{
+    Node<Key, Value>* x = select(root, k - 1);
+    return x->key;
+}
+template<class Key, class Value>
+Node<Key, Value>* bst<Key, Value>::select(Node<Key, Value>* root, int k)
+{
+    if(!root) return NULL;
+    int t = size(root->left);
+    if(t > k) return select(root->left, k);
+    else if(t < k) return select(root->right, k - t - 1);
+    else return root;
+}
+template<class Key, class Value>
+int bst<Key, Value>::rank(Key key)
+{
+    return rank(root, key) + 1;
+}
+template<class Key, class Value>
+int bst<Key, Value>::rank(Node<Key, Value>* root, Key key)
+{
+    if(!root) return 0;
+    if(less(key, root->key)) return rank(root->left, key);
+    else if(more(key, root->key)) return 1 + size(root->left) + rank(root->right, key);
+    else return size(root->left);
+}
+template<class Key, class Value>
+void bst<Key, Value>::deleteMin()
+{
+    root = deleteMin(root);
+}
+template<class Key, class Value>
+Node<Key, Value>* bst<Key, Value>::deleteMin(Node<Key, Value>* root)
+{
+    if(root->left == nullptr) return root->right;
+    root->left = deleteMin(root->left);
+    root->N = size(root->left) + size(root->right) + 1;
+    return root;
+}
+template<class Key, class Value>
+void bst<Key, Value>::deleteMax()
+{
+    root = deleteMax(root);
+}
+template<class Key, class Value>
+Node<Key, Value>* bst<Key, Value>::deleteMax(Node<Key, Value>* root)
+{
+    if(root->right == nullptr) return root->left;
+    root->right = deleteMax(root->right);
+    root->N = size(root->left) + size(root->right) + 1;
+    return root;
+}
+template<class Key, class Value>
+void bst<Key, Value>::Delete(Key key)
+{
+    root = Delete(root, key);
+}
+template<class Key, class Value>
+Node<Key, Value>* bst<Key, Value>::Delete(Node<Key, Value>* root, Key key)
+{
+    if(!root) return nullptr;
+    if(less(key, root->key)) root->left = Delete(root->left, key);
+    else if(more(key, root->key)) root->right = Delete(root->right, key);
+    else
+    {
+        if(root->left == nullptr) return root->right;
+        if(root->right == nullptr) return root->left;
+        Node<Key, Value>* t = root;
+        root = min(root->right);
+        root->right = deleteMin(root->right);
+        root->left = t->left;
+    }
+    root->N = size(root->left) + size(root->right) + 1;
+    return root;
+}
 
 int main()
 {
     bst<char, int> tree;
     tree.put('b', 34);
     tree.put('d', 1);
-    tree.put('a', 1);
+    tree.put('g', 1);
     tree.put('c', 1);
     tree.put('e', 1);
-    // tree.xianxu();
-    cout << tree.get('a');
+    tree.xianxu();
+    tree.Delete('b');
+    tree.xianxu();
     return 0;
 }
